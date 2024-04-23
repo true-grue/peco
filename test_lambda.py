9ae54093c6045017c62a7da1b1f03d17a09b2416
Artyom V. Gorchakov <worldbeater-dev@yandex.ru>
2024-04-23 15:16:06 +0300

    fix: PEP8 issues

--- test_lambda.py
+++ test_lambda.py
@@ -6,15 +6,20 @@ tok = lambda f: memo(seq(ws, f))
 skip = lambda c: tok(sym(c))
 
 atom = tok(cite(letter))
-func = seq(skip('λ'), atom, skip('.'), expr, to(lambda arg, body: ('func', arg, body)))
+func = seq(skip('λ'), atom, skip('.'), expr,
+           to(lambda arg, body: ('func', arg, body)))
 appl = seq(expr, expr, to(lambda a, b: ('apply', a, b)))
 
 brackets = seq(skip('('), expr, skip(')'))
 expr = left(alt(func, appl, atom, brackets))
 
 
-def test():    
+def test():
     x = ' λb. λg. (λa.b g(a))  '
-    y = (('func', 'b', ('func', 'g', ('func', 'a', ('apply', 'b', ('apply', 'g', 'a'))))),)
+    y = (('func', 'b',
+          ('func', 'g',
+           ('func', 'a',
+            ('apply', 'b',
+             ('apply', 'g', 'a'))))),)
     s = parse(x, seq(expr, ws))
     assert s.ok and s.stack == y
