db922fae88f556d60a12b025fc635cc66e1cd113
Artyom V. Gorchakov <worldbeater-dev@yandex.ru>
2024-04-23 14:03:10 +0300

    fix: Improve grammar

--- test_lambda.py
+++ test_lambda.py
@@ -6,16 +6,15 @@ tok = lambda f: memo(seq(ws, f))
 skip = lambda c: tok(sym(c))
 
 atom = tok(cite(letter))
-func = seq(skip('λ'), atom, skip('.'), expr,
-           to(lambda arg, body: ('func', arg, body)))
+func = seq(skip('λ'), atom, skip('.'), expr, to(lambda arg, body: ('func', arg, body)))
+appl = seq(expr, expr, to(lambda a, b: ('apply', a, b)))
 
-part = alt(seq(skip('('), expr, skip(')')), expr)
-appl = seq(part, part, to(lambda a, b: ('apply', a, b)))
-expr = left(alt(func, appl, atom))
+brackets = seq(skip('('), expr, skip(')'))
+expr = left(alt(func, appl, atom, brackets))
 
 
-def test():
-    x = ' λb. λg. λa.b( ga)  '
+def test():    
+    x = ' λb. λg. (λa.b g(a))  '
     y = (('func', 'b', ('func', 'g', ('func', 'a', ('apply', 'b', ('apply', 'g', 'a'))))),)
     s = parse(x, seq(expr, ws))
     assert s.ok and s.stack == y
