c90808a2fee99a088c63a9586623979a912399db
Artyom V. Gorchakov <worldbeater-dev@yandex.ru>
2024-04-23 13:00:59 +0300

    tests: Lambda Calculus

---
+++ test_lambda.py
@@ -0,0 +1,19 @@
+from peco import *
+
+expr = lambda s: expr(s)
+ws = many(space)
+
+atom = seq(cite(letter), to(lambda x: x))
+func = seq(sym('λ'), ws, atom, ws, sym('.'), ws, expr,
+           to(lambda arg, body: ('func', arg, body)))
+
+part = alt(seq(sym('('), ws, expr, ws, sym(')')), expr)
+appl = seq(part, ws, part, to(lambda a, b: ('apply', a, b)))
+expr = left(alt(func, appl, atom))
+
+
+def test():
+    x = 'λb. λg. λa.b( ga)'
+    y = (('func', 'b', ('func', 'g', ('func', 'a', ('apply', 'b', ('apply', 'g', 'a'))))),)
+    s = parse(x, expr)
+    assert s.ok and s.stack == y
