732ad575c592ec6d8818c33a7ebc7c3e93f4223e
Peter Sovietov <peter.sovietov@gmail.com>
2024-10-16 16:36:54 +0300

    new peco, with regexp-based 'eat'

--- test_expr.py
+++ test_expr.py
@@ -3,22 +3,22 @@ from peco import *
 ws = eat(r'\s*')
 tok = lambda f: memo(seq(ws, f))
 skip = lambda c: tok(eat(c))
-op = lambda c: tok(cite(eat(c)))
 
 mkvar = to(lambda x: ('var', x))
 mknum = to(lambda x: ('num', x))
 mkbop = to(lambda a, o, b: (o, a, b))
 
-var = seq(cite(eat(r'[a-zA-Z][a-zA-Z0-9]*')), mkvar)
-num = seq(cite(eat(r'\d+')), mknum)
+var = seq(tok(cite(eat(r'[a-zA-Z][a-zA-Z0-9]*'))), mkvar)
+num = seq(tok(cite(eat(r'\d+'))), mknum)
+op = lambda c: tok(cite(eat(c)))
 
 expr = lambda s: expr(s)
 term = lambda s: term(s)
 
 factor = alt(
     seq(skip(r'\('), expr, skip(r'\)')),
-    tok(var),
-    tok(num)
+    var,
+    num
 )
 
 expr = left(alt(
